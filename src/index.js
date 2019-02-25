650fcc093b3063912924a22bc9ae4599759cef8f
RyanWork <ryanha.mail@gmail.com>
2019-02-25 15:22:16 -0500

    Added game state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/**
+ * Create a square object to render
+ * @param  props    The properties of the square
+ */
 function Square(props){
     return (
         <button 
@@ -10,10 +14,13 @@ function Square(props){
         >
             {props.value}
         </button>
-
     );
 }
 
+/**
+ * Determines if there is a winner
+ * @param squares The state of squares
+ */
 function calculateWinner(squares) {
     // Possible winning positions
     const lines = [
@@ -41,17 +48,60 @@ function calculateWinner(squares) {
 }
 
 class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        }
+    renderSquare(i) {
+        return (
+            <Square 
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
     }
 
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        history: [
+            {
+            squares: Array(9).fill(null)
+            }
+        ],
+        stepNumber: 0,
+        xIsNext: true
+        };
+    }
+
+    /**
+     * Handler for when the board is clicked
+     * @param i the index of square clicked
+     */
     handleClick(i){
-        // Copy the array for immutability
-        const squares = this.state.squares.slice();
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
 
         // Prevent board state change if there is a winner
         if(calculateWinner(squares) || squares[i]){
@@ -63,65 +113,65 @@ class Board extends React.Component {
 
         // Set the state of the array
         this.setState({
-            squares: squares,
+            history: history.concat([{
+                squares: squares
+              }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
         });
     }
-
-    renderSquare(i) {
-        return (
-            <Square 
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
+    
+    /**
+     * Handler for when a user clicks a jump button
+     * Sets the state to the mapped state
+     * @param step  step number
+     */
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        });
     }
 
     render() {
-        // Check if there is a winner
-        const winner = calculateWinner(this.state.squares);
+        // Create some local scope variables
+        // Track the history of states, the current state, and winner state
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        // Move the move number to a past state
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                // Render a button to press
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        // Given winner state, display current state of turn
         let status;
-        if (winner)
+        if (winner) {
             status = 'Winner: ' + winner;
-        else 
+        } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+        }
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
+            <div className="game">
+                <div className="game-board">
+                <Board 
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+                </div>
+                <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+                </div>
             </div>
-            <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-            </div>
-        </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-        <div className="game">
-            <div className="game-board">
-            <Board />
-            </div>
-            <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-            </div>
-        </div>
         );
     }
 }
@@ -129,6 +179,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-<Game />,
-document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
